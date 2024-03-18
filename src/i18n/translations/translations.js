affed2b7273f22a13fa4c4dd27f18beaa120e507
Sondre Njåstad <sondre.njaastad@gmail.com>
2024-03-18 18:45:19 +0100

    add languages to prepare for rebase

--- src/i18n/translations/translations.js
+++ src/i18n/translations/translations.js
@@ -1,23 +1,29 @@
+import bn from './bn.js'
 import da from './da.js';
 import de from './de.js'
 import en from './en.js';
 import fa from './fa.js';
+import fi from './fi.js';
 import fr from './fr.js';
 import ko from './ko.js';
 import nb from './nb.js';
 import nn from './nn.js';
+import ro from './ro.js';
 import sv from './sv.js';
 import zh from './zh.js';
 
 export default {
+    bn,
     da,
     de,
     en,
     fa,
+    fi,
     fr,
     ko,
     nb,
     nn,
+    ro,
     sv,
     zh,
 };
