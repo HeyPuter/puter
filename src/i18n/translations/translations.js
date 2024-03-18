b3472d71c59e4c28fbd291a951e9aafe7310cdab
Sondre Njåstad <sondre.njaastad@gmail.com>
2024-03-18 14:11:41 +0100

    Alphabetical order

--- src/i18n/translations/translations.js
+++ src/i18n/translations/translations.js
@@ -1,21 +1,21 @@
+import da from './da.js';
 import en from './en.js';
-import nb from './nb.js';
 import fa from './fa.js';
-import ko from './ko.js';
-import zh from './zh.js';
 import fr from './fr.js';
+import ko from './ko.js';
+import nb from './nb.js';
 import nn from './nn.js';
 import sv from './sv.js';
-import da from './da.js';
+import zh from './zh.js';
 
 export default {
+    da,
     en,
-    nb,
     fa,
-    ko,
-    zh,
     fr,
+    ko,
+    nb,
     nn,
     sv,
-    da,
+    zh,
 };
