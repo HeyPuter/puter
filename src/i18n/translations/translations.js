b8c63f03824425c529e7c46a9113758b2d286f48
Sondre Njåstad <sondre.njaastad@gmail.com>
2024-03-18 13:33:05 +0100

    more translation work

--- src/i18n/translations/translations.js
+++ src/i18n/translations/translations.js
@@ -1,15 +1,17 @@
 import en from './en.js';
-import no from './no.js';
+import nb from './nb.js';
 import fa from './fa.js';
 import ko from './ko.js';
 import zh from './zh.js';
 import fr from './fr.js';
+import nn from './nn.js'
 
 export default {
-    en: en,
-    no: no,
-    fa: fa,
-    ko: ko,
-    zh: zh,
-    fr: fr,
+    en,
+    nb,
+    fa,
+    ko,
+    zh,
+    fr,
+    nn,
 };
