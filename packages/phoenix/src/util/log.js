c9a8c23d5070900626db5fb1a89364ce3ca10e74
KernelDeimos <eric.alex.dube@gmail.com>
2024-04-17 13:14:03 -0400

    Update license header

--- packages/phoenix/src/util/log.js
+++ packages/phoenix/src/util/log.js
@@ -5,8 +5,7 @@
  *
  * Phoenix Shell is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * by the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
