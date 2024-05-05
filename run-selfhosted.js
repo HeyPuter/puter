551121524c40114caf51d30cf8cbaa77f03f9202
Didi Keke <nyedidikeke@users.noreply.github.com>
2024-05-05 07:53:55 +0000

    refactor: name from `SelfhostedModule` to `SelfHostedModule` :hammer:

--- run-selfhosted.js
+++ run-selfhosted.js
@@ -59,7 +59,7 @@ const main = async () => {
         DatabaseModule,
         PuterDriversModule,
         LocalDiskStorageModule,
-        SelfhostedModule
+        SelfHostedModule
     } = (await import('@heyputer/backend')).default;
 
     console.log('kerne', Kernel);
@@ -68,7 +68,7 @@ const main = async () => {
     k.add_module(new DatabaseModule());
     k.add_module(new PuterDriversModule());
     k.add_module(new LocalDiskStorageModule());
-    k.add_module(new SelfhostedModule());
+    k.add_module(new SelfHostedModule());
     k.boot();
 };
 
