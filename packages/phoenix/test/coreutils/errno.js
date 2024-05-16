017077f8cbf796ac9391dc6bb54474e3471fb4fa
Sam Atkins <atkinssj@gmail.com>
2024-05-16 10:29:14 +0100

    Move error-conversion functions into PosixError
    Specifically, this makes the Puter->PosixError conversion available to
    the in-progress git client.

--- packages/phoenix/test/coreutils/errno.js
+++ packages/phoenix/test/coreutils/errno.js
@@ -100,7 +100,8 @@ export const runErrnoTests = () => {
                     'EADDRINUSE    98 Address already in use\n' +
                     'ECONNRESET   104 Connection reset\n' +
                     'ETIMEDOUT    110 Connection timed out\n' +
-                    'ECONNREFUSED 111 Connection refused\n',
+                    'ECONNREFUSED 111 Connection refused\n' +
+                    'EUNKNOWN      -1 Unknown error\n',
                 expectedStderr: '',
                 expectedFail: false,
             },
