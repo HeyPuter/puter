2f4882c3d7bbb5242b29e0c20239456640c1e724
Sam Atkins <atkinssj@gmail.com>
2024-05-31 17:49:19 +0100

    feat(git): Implement `git clone`

--- packages/git/src/git-helpers.js
+++ packages/git/src/git-helpers.js
@@ -18,6 +18,8 @@
  */
 import path from 'path-browserify';
 
+export const PROXY_URL = 'https://cors.isomorphic-git.org';
+
 /**
  * Attempt to locate the git repository directory.
  * @throws Error If no git repository could be found, or another error occurred.
