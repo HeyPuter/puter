98a4b9ede39b94c0c6b6b8345d7551359961186a
Sam Atkins <atkinssj@gmail.com>
2024-06-04 14:20:40 +0100

    feat(git): Implement `git fetch`
    Sporadically we hang while trying to fetch. I haven't been able to
    identify why but it seems like a race condition in isomorphic-git
    somewhere.

---
+++ packages/git/src/subcommands/fetch.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2024  Puter Technologies Inc.
+ *
+ * This file is part of Puter's Git client.
+ *
+ * Puter's Git client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import git from 'isomorphic-git';
+import http from 'isomorphic-git/http/web';
+import { determine_fetch_remote, find_repo_root, PROXY_URL } from '../git-helpers.js';
+import { SHOW_USAGE } from '../help.js';
+
+export default {
+    name: 'fetch',
+    usage: [
+        'git fetch <repository>',
+        'git fetch --all',
+    ],
+    description: `Download objects and refs from another repository.`,
+    args: {
+        allowPositionals: true,
+        options: {
+            all: {
+                description: 'Fetch all remotes.',
+                type: 'boolean',
+                default: false,
+            }
+        },
+    },
+    execute: async (ctx) => {
+        const { io, fs, env, args } = ctx;
+        const { stdout, stderr } = io;
+        const { options, positionals } = args;
+        const cache = {};
+
+        const { repository_dir, git_dir } = await find_repo_root(fs, env.PWD);
+
+        // TODO: Support <refspec> syntax.
+
+        const remotes = await git.listRemotes({
+            fs,
+            dir: repository_dir,
+            gitdir: git_dir,
+        });
+
+        if (options.all) {
+            for (const { remote, url } of remotes) {
+                stdout(`Fetching ${remote}\nFrom ${url}`);
+                await git.fetch({
+                    fs,
+                    http,
+                    cache,
+                    corsProxy: PROXY_URL,
+                    dir: repository_dir,
+                    gitdir: git_dir,
+                    remote,
+                    onMessage: (message) => { stdout(message); },
+                });
+            }
+            return;
+        }
+
+        const remote = positionals.shift();
+        const remote_data = determine_fetch_remote(remote, remotes);
+
+        await git.fetch({
+            fs,
+            http,
+            cache,
+            corsProxy: PROXY_URL,
+            dir: repository_dir,
+            gitdir: git_dir,
+            ...remote_data,
+            onMessage: (message) => { stdout(message); },
+        });
+    }
+}
