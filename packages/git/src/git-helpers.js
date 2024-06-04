ae5ae434ac979897f804e1cb92d46246b19ee9cd
Sam Atkins <atkinssj@gmail.com>
2024-06-04 14:20:40 +0100

    feat(git): Implement `git fetch`
    Sporadically we hang while trying to fetch. I haven't been able to
    identify why but it seems like a race condition in isomorphic-git
    somewhere.

--- packages/git/src/git-helpers.js
+++ packages/git/src/git-helpers.js
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import path from 'path-browserify';
+import git from 'isomorphic-git';
 
 export const PROXY_URL = 'https://cors.isomorphic-git.org';
 
@@ -83,4 +84,29 @@ export const find_repo_root = async (fs, pwd) => {
 export const shorten_hash = (hash) => {
     // TODO: Ensure that whatever we produce is unique within the repo
     return hash.slice(0, 7);
+}
+
+/**
+ * Determine the remot/url parameters to pass to git.fetch(), based on a `<repository>` string.
+ * @param remote_name_or_url Command-line parameter, either a remote name, an url, or undefined.
+ * @param remotes List of all existing remotes, from `git.listRemotes()`
+ * @returns {remote, url} Object with fields to pass to git.fetch() or similar.
+ */
+export const determine_fetch_remote = (remote_name_or_url, remotes) => {
+    if (!remote_name_or_url) {
+        // We leave `url` and `remote` blank and git.fetch() handles the default.
+        return {};
+    }
+
+    if (URL.canParse(remote_name_or_url)) {
+        return { url: remote_name_or_url };
+    }
+
+    // Named remote. First, check if the remote exists. `git.fetch` reports non-existent remotes as:
+    //     "The function requires a "remote OR url" parameter but none was provided."
+    // ...which is not helpful to the user.
+    const remote_data = remotes.find(it => it.remote === remote_name_or_url);
+    if (!remote_data)
+        throw new Error(`'${remote_name_or_url}' does not appear to be a git repository`);
+    return remote_data;
 }
