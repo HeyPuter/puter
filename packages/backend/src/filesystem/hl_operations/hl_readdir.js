a72ec9799ac3bd76ceafa22cce149e373a13f3b9
KernelDeimos <eric.alex.dube@gmail.com>
2024-06-22 19:10:25 -0400

    fix: readdir on file

--- packages/backend/src/filesystem/hl_operations/hl_readdir.js
+++ packages/backend/src/filesystem/hl_operations/hl_readdir.js
@@ -38,7 +38,7 @@ class HLReadDir extends HLFilesystemOperation {
             if ( ! await svc_acl.check(actor, subject, 'see') ) {
                 throw await svc_acl.get_safe_acl_error(actor, subject, 'see');
             }
-            return [subject];
+            return [await subject.getSafeEntry()];
         }
         
         let children;
