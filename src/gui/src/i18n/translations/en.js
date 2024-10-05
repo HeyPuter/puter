f3625ed907a704ff074ed485b0aa21fdb663a582
4nshuman <anshuman01.tripathi@gmail.com>
2024-10-05 23:43:43 +0530

    added terms for status message on zip operation

--- src/gui/src/i18n/translations/en.js
+++ src/gui/src/i18n/translations/en.js
@@ -226,7 +226,8 @@ const en = {
         publish: "Publish",
         publish_as_website: 'Publish as website',
         puter_description: `Puter is a privacy-first personal cloud to keep all your files, apps, and games in one secure place, accessible from anywhere at any time.`,
-        reading_file: "Reading %strong%",
+        reading: "Reading %strong%",
+        writing: "Writing %strong%",
         recent: "Recent",
         recommended: "Recommended",
         recover_password: "Recover Password",
@@ -316,7 +317,8 @@ const en = {
         yes_release_it: 'Yes, Release It',
         you_have_been_referred_to_puter_by_a_friend: "You have been referred to Puter by a friend!",
         zip: "Zip",
-        zipping_file: "Zipping %strong%",
+        sequencing: "Sequencing %strong%",
+        zipping: "Zipping %strong%",
 
         // === 2FA Setup ===
         setup2fa_1_step_heading: 'Open your authenticator app',      
