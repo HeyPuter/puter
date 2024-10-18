9b1f1817f5adbd373d1c5c6f9d27916e20c84f22
KernelDeimos <eric.alex.dube@gmail.com>
2024-10-17 21:52:17 -0400

    dev: move service interface to trait

--- src/putility/src/concepts/Service.js
+++ src/putility/src/concepts/Service.js
@@ -2,6 +2,9 @@ const { AdvancedBase } = require("../AdvancedBase");
 
 const NOOP = async () => {};
 
+/** Service trait */
+const TService = Symbol('TService');
+
 /**
  * Service will be incrementally updated to consolidate
  * BaseService in Puter's backend with Service in Puter's frontend,
@@ -23,23 +26,27 @@ class Service extends AdvancedBase {
     static create ({ parameters, context }) {
         const ins = new this();
         ins._.context = context;
-        ins.construct(parameters);
+        ins.as(TService).construct(parameters);
         return ins;
     }
 
-    init (...a) {
-        if ( ! this._init ) return;
-        return this._init(...a);
-    }
-
-    construct (o) {
-        this.$parameters = {};
-        for ( const k in o ) this.$parameters[k] = o[k];
-        if ( ! this._construct ) return;
-        return this._construct(o);
+    static IMPLEMENTS = {
+        [TService]: {
+            init (...a) {
+                if ( ! this._init ) return;
+                return this._init(...a);
+            },
+            construct (o) {
+                this.$parameters = {};
+                for ( const k in o ) this.$parameters[k] = o[k];
+                if ( ! this._construct ) return;
+                return this._construct(o);
+            }
+        }
     }
 }
 
 module.exports = {
+    TService,
     Service,
 };
