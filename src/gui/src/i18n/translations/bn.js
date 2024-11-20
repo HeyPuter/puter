86a54b9415f70b520a00c73a9e8c5fb418e5bb42
FahimFBA <fahimbinamin@gmail.com>
2024-11-20 21:43:36 +0600

    add translation: password_recovery_token_invalid

--- src/gui/src/i18n/translations/bn.js
+++ src/gui/src/i18n/translations/bn.js
@@ -354,7 +354,7 @@ const bn = {
     // ***********************************
     "change": "পরিবর্তন", // In English: "Change"
     "clock_visibility": "ঘড়ির দৃশ্যমানতা", // In English: "Clock Visibility"
-    "password_recovery_token_invalid": undefined, // In English: "This password recovery token is no longer valid."
+    "password_recovery_token_invalid": "এই পাসওয়ার্ড রিকোভারি টোকেনটি আর ব্যবহারযোগ্য নয়।", // In English: "This password recovery token is no longer valid."
     "password_recovery_unknown_error": undefined, // In English: "An unknown error occurred. Please try again later."
     "password_required": undefined, // In English: "Password is required."
     "password_strength_error": undefined, // In English: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."
