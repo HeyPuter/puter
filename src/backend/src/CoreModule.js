c5cd8cc21984e1912d587e2c95636400ceadf574
KernelDeimos <eric.alex.dube@gmail.com>
2024-11-27 16:29:57 -0500

    dev: register PermissionUtil

--- src/backend/src/CoreModule.js
+++ src/backend/src/CoreModule.js
@@ -58,6 +58,7 @@ const install = async ({ services, app, useapi, modapi }) => {
         def('Library', Library);
 
         def('core.util.helpers', require('./helpers'));
+        def('core.util.permission', require('./services/auth/PermissionService').PermissionUtil);
         def('puter.middlewares.auth', require('./middleware/auth2'));
         def('puter.middlewares.anticsrf', require('./middleware/anticsrf'));
         
