2ca1f71595b1bbb87f84dc712892f92dc842a82b
e5z26 <122952345+e5z26@users.noreply.github.com>
2024-12-07 19:09:22 -0500

    removed jsdom in test file
    removed because wasn't being used

--- src/phoenix/test/test-git-detection.js
+++ src/phoenix/test/test-git-detection.js
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import assert from 'assert';
-import { JSDOM } from 'jsdom';
 
 // Function to test
 async function hasGitDirectory(items) {
