3f5bc7163f317fb3f986cd61f4888a5d94cd9ef7
KernelDeimos <eric.alex.dube@gmail.com>
2024-12-09 14:08:46 -0500

    dev: cleanup config.js

--- src/backend/src/config.js
+++ src/backend/src/config.js
@@ -31,24 +31,24 @@ config.disable_temp_users = false;
 config.default_user_group = '78b1b1dd-c959-44d2-b02c-8735671f9997';
 config.default_temp_group = 'b7220104-7905-4985-b996-649fdcdb3c8f';
 
-config.max_file_size = 100_000_000_000,
-config.max_thumb_size = 1_000,
-config.max_fsentry_name_length = 767,
+config.max_file_size = 100_000_000_000;
+config.max_thumb_size = 1_000;
+config.max_fsentry_name_length = 767;
 
-config.username_regex = /^\w{1,}$/;
+config.username_regex = /^\w+$/;
 config.username_max_length = 45;
-config.subdomain_regex = /^[a-zA-Z0-9-_-]+$/;
+config.subdomain_regex = /^[a-zA-Z0-9_-]+$/;
 config.subdomain_max_length = 60;
-config.app_name_regex = /^[a-zA-Z0-9-_-]+$/;
+config.app_name_regex = /^[a-zA-Z0-9_-]+$/;
 config.app_name_max_length = 60;
 config.app_title_max_length = 60;
 config.min_pass_length = 6;
 
-config.strict_email_verification_required = false,
-config.require_email_verification_to_publish_website = false,
+config.strict_email_verification_required = false;
+config.require_email_verification_to_publish_website = false;
 
-config.kv_max_key_size = 1024,
-config.kv_max_value_size = 400 * 1024,
+config.kv_max_key_size = 1024;
+config.kv_max_value_size = 400 * 1024;
 
 config.monitor = {
     metricsInterval: 60000,
@@ -70,9 +70,6 @@ config.app_max_icon_size = 5*1024*1024;
 
 config.defaultjs_asset_path = '../../';
 
-// config.origin = config.protocol + '://' + config.domain;
-// config.api_base_url = config.protocol + '://api.' + config.domain;
-// config.social_card = `${config.origin}/assets/img/screenshot.png`;
 config.short_description = `Puter is a privacy-first personal cloud that houses all your files, apps, and games in one private and secure place, accessible from anywhere at any time.`;
 config.title = 'Puter';
 config.company = 'Puter Technologies Inc.';
@@ -96,11 +93,12 @@ config.reserved_words = [];
 // set default S3 settings for this server, if any
 if (config.server_id) {
 	// see if this server has a specific bucket
-	for (let index = 0; index < config.servers.length; index++) {
-		if (config.servers[index].id === config.server_id && config.servers[index].s3_bucket){
-			config.s3_bucket = config.servers[index].s3_bucket;
-            config.s3_region = config.servers[index].region;
-        }
+    for ( const server of config.servers ) {
+        if ( server.id !== config.server_id ) continue;
+        if ( ! server.s3_bucket ) continue;
+
+        config.s3_bucket = server.s3_bucket;
+        config.s3_region = server.region;
 	}
 }
 
@@ -173,7 +171,6 @@ const config_pointer = {};
         let replacement_config = {
             ...o,
         };
-        // replacement_config.__proto__ = config_pointer.__proto__;
         replacement_config = deep_proto_merge(replacement_config, config_pointer.__proto__, {
             preserve_flag: true,
         })
