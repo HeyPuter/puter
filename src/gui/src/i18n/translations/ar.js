15898fe5b744a3c738df6fd69c0a8a93083e886a
KernelDeimos <eric.alex.dube@gmail.com>
2025-01-06 11:55:17 -0500

    chore: update license headers

--- src/gui/src/i18n/translations/ar.js
+++ src/gui/src/i18n/translations/ar.js
@@ -1,19 +1,22 @@
-/** * Copyright (C) 2024 Puter Technologies Inc. *
+/*
+ * Copyright (C) 2024 Puter Technologies Inc.
+ * 
  * This file is part of Puter.
- *
+ * 
  * Puter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 const ar = {
   name: "العربية",
--- src/gui/src/util/desktop.js
+++ src/gui/src/util/desktop.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2024 Puter Technologies Inc.
+ * 
+ * This file is part of Puter.
+ * 
+ * Puter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * This file contains functions that are used by Puter's desktop GUI.
  * Functions moved here are not bound to the `window` object, making it
