8fabf014a9eb783183e87489ae2b6c6bbc42c99a
KernelDeimos <eric.alex.dube@gmail.com>
2025-01-09 19:07:19 -0500

    fix: circular imports

--- src/backend/src/modules/selfhosted/ComplainAboutVersionsService.js
+++ src/backend/src/modules/selfhosted/ComplainAboutVersionsService.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-const { BaseService } = require("../../../exports");
+const BaseService = require('../../services/BaseService');
 const { surrounding_box } = require("../../fun/dev-console-ui-utils");
 
 class ComplainAboutVersionsService extends BaseService {
--- src/backend/src/services/HostDiskUsageService.js
+++ src/backend/src/services/HostDiskUsageService.js
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-const { BaseService } = require("../../exports");
+const BaseService = require('./BaseService');
 const { execSync } = require('child_process');
 const config = require("../config");
 
