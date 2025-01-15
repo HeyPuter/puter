5798fcd0421912c2816f1b3500cda3ef5f11533a
ProgrammerIn-wonderland <3838shah@gmail.com>
2025-01-15 17:12:42 -0500

    automatic reconnection on disconnect

--- src/puter-js/src/modules/networking/PWispHandler.js
+++ src/puter-js/src/modules/networking/PWispHandler.js
@@ -6,34 +6,40 @@ export class PWispHandler {
     _bufferMax;
     streamMap = new Map();
     constructor(wispURL, puterAuth) {
-        this._ws = new WebSocket(wispURL);
-        this._ws.binaryType = "arraybuffer"
-        this._ws.onmessage = (event) => {
-            const parsed = parseIncomingPacket(new Uint8Array(event.data));
-            switch (parsed.packetType) {
-                case DATA:
-                    this.streamMap.get(parsed.streamID).dataCallBack(parsed.payload.slice(0)) // return a copy for the user to do as they please
-                    break;
-                case CONTINUE:
-                    if (parsed.streamID === 0) {
-                        this._bufferMax = parsed.remainingBuffer;
-                        return;
-                    }
-                    this.streamMap.get(parsed.streamID).buffer = parsed.remainingBuffer;
-                    this._continue()
-                    break;
-                case CLOSE:
-                    this.streamMap.get(parsed.streamID).closeCallBack(parsed.reason);
-                    break;
-                case INFO:
-                    puterAuth && this._ws.send(createWispPacket({
-                        packetType: INFO,
-                        streamID: 0,
-                        puterAuth
-                    }))
-                    break;
+        const setup = () => {
+            this._ws = new WebSocket(wispURL);
+            this._ws.binaryType = "arraybuffer"
+            this._ws.onmessage = (event) => {
+                const parsed = parseIncomingPacket(new Uint8Array(event.data));
+                switch (parsed.packetType) {
+                    case DATA:
+                        this.streamMap.get(parsed.streamID).dataCallBack(parsed.payload.slice(0)) // return a copy for the user to do as they please
+                        break;
+                    case CONTINUE:
+                        if (parsed.streamID === 0) {
+                            this._bufferMax = parsed.remainingBuffer;
+                            this._ws.onclose = () => {
+                                setTimeout(setup(), 1000);
+                            }
+                            return;
+                        }
+                        this.streamMap.get(parsed.streamID).buffer = parsed.remainingBuffer;
+                        this._continue()
+                        break;
+                    case CLOSE:
+                        this.streamMap.get(parsed.streamID).closeCallBack(parsed.reason);
+                        break;
+                    case INFO:
+                        puterAuth && this._ws.send(createWispPacket({
+                            packetType: INFO,
+                            streamID: 0,
+                            puterAuth
+                        }))
+                        break;
+                }
             }
         }
+        setup();
     }
     _continue(streamID) {
         const queue = this.streamMap.get(streamID).queue;
