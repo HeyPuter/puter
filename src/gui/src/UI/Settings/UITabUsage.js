f7a8c0ac6c9ceb84aaee23d18e13a228282cee03
KernelDeimos <eric.alex.dube@gmail.com>
2025-04-04 18:43:05 -0400

    dev: instant update for usage in settings

--- src/gui/src/UI/Settings/UITabUsage.js
+++ src/gui/src/UI/Settings/UITabUsage.js
@@ -41,6 +41,7 @@ export default {
             </div>`;
     },
     init: ($el_window) => {
+        const sanitize_id = id => (''+id).replace(/[^A-Za-z0-9-]/g, '');
         $.ajax({
             url: window.api_origin + "/drivers/usage",
             type: 'GET',
@@ -79,18 +80,46 @@ export default {
                         name = `File Conversions`;
 
                     h += `
-                        <div class="driver-usage" style="margin-bottom: 10px;">
+                        <div
+                            class="driver-usage"
+                            style="margin-bottom: 10px;"
+                            data-id="${sanitize_id(entry.id)}"
+                        >
                             <h3 style="margin-bottom: 5px; font-size: 14px;">${html_encode(name)}:</h3>
-                            <span style="font-size: 13px; margin-bottom: 3px;">${Number(entry.used)} used of ${Number(entry.available)}</span>
+                            <span style="font-size: 13px; margin-bottom: 3px;">${i18n('used_of', entry)}</span>
                             <div class="usage-progbar-wrapper" style="width: 100%;">
                                 <div class="usage-progbar" style="width: ${Number(entry.usage_percentage)}%;"><span class="usage-progbar-percent">${Number(entry.usage_percentage)}%</span></div>
                             </div>
                         </div>
                     `;
                 });
+                
+                const divContent = $('.settings-content[data-settings="usage"]');
 
                 // Append driver usage bars to the container
-                $('.settings-content[data-settings="usage"]').append(`<div class="driver-usage-container">${h}</div>`);
+                divContent.append(`<div class="driver-usage-container">${h}</div>`);
+                
+                const update_usage = event => {
+                    if ( ! event.usage_percentage ) {
+                        event.usage_percentage = (event.used / event.available * 100).toFixed(0);
+                    }
+                    const el_divContent = divContent[0];
+                    el_divContent
+                        .querySelector(`[data-id=${sanitize_id(event.id)}] .usage-progbar`)
+                        .style.width = '' + Number(event.usage_percentage) + '%';
+                    el_divContent
+                        .querySelector(`[data-id=${sanitize_id(event.id)}] .usage-progbar span`)
+                        .innerText = '' + Number(event.usage_percentage) + '%';
+                    const used_of_str = i18n('used_of', event);
+                    el_divContent
+                        .querySelector(`[data-id=${sanitize_id(event.id)}] > span`)
+                        .innerText = used_of_str;
+                };
+
+                divContent.on('remove', () => {
+                    socket.off('usage.update', update_usage);
+                });
+                socket.on('usage.update', update_usage);
             }
         });
 
