32c8bcde1e60adcefe4c8dccb08d40c6551e102d
KernelDeimos <eric.alex.dube@gmail.com>
2025-05-09 15:27:25 -0400

    dev: custom domain routing (the easy part)
    With this commit alone, any user can specify "domain" on their
    subdomains. This means they could affect the route for domains they
    don't control, so this is not production-ready without further changes.

--- src/backend/src/services/PuterSiteService.js
+++ src/backend/src/services/PuterSiteService.js
@@ -113,15 +113,18 @@ class PuterSiteService extends BaseService {
     * @returns {Promise<Object|null>} Returns an object with subdomain details or null if not found.
     * @note In development environment, 'devtest' subdomain returns hardcoded values.
     */
-    async get_subdomain (subdomain) {
+    async get_subdomain (subdomain, options) {
         if ( subdomain === 'devtest' && this.global_config.env === 'dev' ) {
             return {
                 user_id: null,
                 root_dir_id: this.config.devtest_directory,
             };
         }
+        console.log('???', subdomain, options);
         const rows = await this.db.read(
-            `SELECT * FROM subdomains WHERE subdomain = ? LIMIT 1`,
+            `SELECT * FROM subdomains WHERE ${
+                options.is_custom_domain ? 'domain' : 'subdomain'
+            } = ? LIMIT 1`,
             [subdomain]
         );
         if ( rows.length === 0 ) return null;
