b0666e34a38f1eec0a89dfefd273168fe90b6a1f
KernelDeimos <eric.alex.dube@gmail.com>
2025-05-14 15:59:01 -0400

    dev: add TXT record verification for domain names

--- src/backend/src/modules/domain/DomainVerificationService.js
+++ src/backend/src/modules/domain/DomainVerificationService.js
@@ -2,6 +2,9 @@ const { get_user } = require("../../helpers");
 const BaseService = require("../../services/BaseService");
 
 class DomainVerificationService extends BaseService {
+    _init () {
+        this._register_commands();
+    }
     async get_controlling_user ({ domain }) {
         const svc_event = this.services.get('event');
         
@@ -18,6 +21,20 @@ class DomainVerificationService extends BaseService {
         // 2 :: If there is no controlling user, 'admin' is the
         //      controlling user.
         return await get_user({ username: 'admin' });
+    }
+
+    _register_commands (commands) {
+        const svc_commands = this.services.get('commands');
+        svc_commands.registerCommands('domain', [
+            {
+                id: 'user',
+                description: '',
+                handler: async (args, log) => {
+                    const res = await this.get_controlling_user({ domain: args[0] });
+                    log.log(res);
+                }
+            }
+        ]);
     }
 }
 
