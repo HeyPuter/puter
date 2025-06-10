1652fb496c6ad2e46c40591677011e99efd69391
Nariman Jelveh <nj@puter.com>
2025-06-10 15:13:12 -0700

    Delete experiments directory

--- experiments/x86emu/imagegen/config/networking.sh
+++
@@ -1,5 +0,0 @@
-rmmod ne2k-pci && modprobe ne2k-pci
-ifconfig enp0s5 192.168.1.5 netmask 255.255.255.0 up
-route add default gw 192.168.1.1
-echo "nameserver 1.1.1.1" > /etc/resolv.conf
-dhcpcd -w4 enp0s5
