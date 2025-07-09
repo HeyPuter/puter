1ffbbf1f8123e48a5ea5b106d4524fcc81c48a9b
KernelDeimos <eric.alex.dube@gmail.com>
2025-07-09 19:58:23 -0400

    refactor: add airouter.js package
    - create new package
    - copy streaming utilities to new package

---
+++ src/airouter.js/airouter.js
@@ -0,0 +1,6 @@
+// Streaming Utilities
+export { CompletionWriter } from './common/stream/CompletionWriter.js';
+export { MessageWriter } from './common/stream/MessageWriter.js';
+export { ToolUseWriter } from './common/stream/ToolUseWriter.js';
+export { TextWriter } from './common/stream/TextWriter.js';
+export { BaseWriter } from './common/stream/BaseWriter.js';
