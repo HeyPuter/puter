b345aa82871ff32b9e95fe602ac5ff6e83d4aa36
KernelDeimos <eric.alex.dube@gmail.com>
2025-07-10 12:43:42 -0400

    refactor: move Messages to the new airouter module
    The Messages library had overloaded concerns and has now been split into
    UniversalPromptNormalizer (normalization for the "universal" input
    format), and NormalizedPromptUtil (functions that can be used on
    normalized prompt input for convenient inference).

--- src/airouter.js/common/prompt/UniversalPromptNormalizer.js
+++ src/airouter.js/common/prompt/UniversalPromptNormalizer.js
@@ -1,6 +1,15 @@
-const { whatis } = require("../../../util/langutil");
+import { whatis } from "../util/lang.js";
 
-module.exports = class Messages {
+/**
+ * UniversalPromptNormalizer is a PromptNormalizer which consumes "prompts"
+ * (arrays of messages) in a format called "universal", and coerces them into
+ * a format called "normalized".
+ * 
+ * In plain terms, this is the code responsible for taking the input JSON
+ * for AI chat and standardizing it into a format that the model-specific
+ * prompt adapters can understand.
+ */
+export class UniversalPromptNormalizer {
     static normalize_single_message (message, params = {}) {
         params = Object.assign({
             role: 'user',
@@ -105,45 +114,5 @@ module.exports = class Messages {
         }
 
         return merged_messages;
-    }
-
-    static extract_and_remove_system_messages (messages) {
-        let system_messages = [];
-        let new_messages = [];
-        for ( let i=0 ; i < messages.length ; i++ ) {
-            if ( messages[i].role === 'system' ) {
-                system_messages.push(messages[i]);
-            } else {
-                new_messages.push(messages[i]);
-            }
-        }
-        return [system_messages, new_messages];
-    }
-
-    static extract_text (messages) {
-        return messages.map(m => {
-            if ( whatis(m) === 'string' ) {
-                return m;
-            }
-            if ( whatis(m) !== 'object' ) {
-                return '';
-            }
-            if ( whatis(m.content) === 'array' ) {
-                return m.content.map(c => c.text).join(' ');
-            }
-            if ( whatis(m.content) === 'string' ) {
-                return m.content;
-            } else {
-                const is_text_type = m.content.type === 'text' ||
-                    ! m.content.hasOwnProperty('type');
-                if ( is_text_type ) {
-                    if ( whatis(m.content.text) !== 'string' ) {
-                        throw new Error('text content must be a string');
-                    }
-                    return m.content.text;
-                }
-                return '';
-            }
-        }).join(' ');
     }
 }
