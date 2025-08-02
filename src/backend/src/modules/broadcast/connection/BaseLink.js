40a3d71214e98728367ed4a22a98a982401e3877
Yasir Rafique <Yasirrafique223@gmail.com>
2025-08-02 07:22:30 +0100

    chore: apply ESLint and Prettier formatting to broadcast module

--- src/backend/src/modules/broadcast/connection/BaseLink.js
+++ src/backend/src/modules/broadcast/connection/BaseLink.js
@@ -1,29 +1,28 @@
 /*
  * Copyright (C) 2024-present Puter Technologies Inc.
- * 
+ *
  * This file is part of Puter.
- * 
+ *
  * Puter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const { AdvancedBase } = require("@heyputer/putility");
-const { ChannelFeature } = require("../../../traits/ChannelFeature");
+const { AdvancedBase } = require('@heyputer/putility');
+
+const { ChannelFeature } = require('../../../traits/ChannelFeature');
 
 class BaseLink extends AdvancedBase {
-    static FEATURES = [
-        new ChannelFeature(),
-    ];
+    static FEATURES = [new ChannelFeature()];
     static CHANNELS = ['message'];
 
     static MODULES = {
@@ -34,15 +33,15 @@ class BaseLink extends AdvancedBase {
     static ONLINE = {};
     static OFFLINE = {};
 
-    send (data) {
-        if ( this.state !== this.constructor.ONLINE ) {
+    send(data) {
+        if (this.state !== this.constructor.ONLINE) {
             return false;
         }
 
         return this._send(data);
     }
 
-    constructor () {
+    constructor() {
         super();
         this.state = this.constructor.AUTHENTICATING;
     }
