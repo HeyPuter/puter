3dc794b10ed85f68660624febf1a022844dc9c93
XiaochenCui <jcnlcxc.new@gmail.com>
2025-08-15 10:07:21 -0700

    fs: add benchmark for stat-intensive scenario

--- tools/api-tester/benches/__entry__.js
+++ tools/api-tester/benches/__entry__.js
@@ -1,3 +1,4 @@
 module.exports = registry => {
-    registry.add_bench('write-intensive-1', require('./write_intensive_1.js'));
+    registry.add_bench('write_intensive_1', require('./write_intensive_1.js'));
+    registry.add_bench('stat_intensive_1', require('./stat_intensive_1.js'));
 };
---
+++ tools/api-tester/benches/stat_intensive_1.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+chai.use(require('chai-as-promised'))
+const expect = chai.expect;
+
+module.exports = {
+    name: 'stat intensive 1',
+    description: 'create 10 directories and 100 subdirectories in each, then stat them intensively',
+    do: async t => {
+        console.log('stat intensive 1');
+
+        const dir_count = 10;
+        const subdir_count = 100;
+
+        // key: uuid
+        // value: path
+        const dirs = {};
+
+        for (let i = 0; i < dir_count; i++) {
+            await t.mkdir(`dir_${i}`);
+            for (let j = 0; j < subdir_count; j++) {
+                const subdir = await t.mkdir(`dir_${i}/subdir_${j}`);
+                dirs[subdir.uid] = subdir.path;
+            }
+        }
+
+        for (let i = 0; i < 10; i++) {
+            for (const [uuid, path] of Object.entries(dirs)) {
+                const stat = await t.stat_uuid(uuid);
+                expect(stat.is_dir).equal(true);
+                expect(stat.path).equal(path);
+            }
+        }
+    }
+};
