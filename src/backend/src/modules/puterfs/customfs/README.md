1f6bbe16723a541311d77699e379e198d34527b0
Xiaochen Cui <jcnlcxc.new@gmail.com>
2025-08-28 02:12:31 +0800

    dev: memory filesystem
    * fs/memory-provider: passed all apitests
    
    * test: write benchmark
    
    * fs: add benchmark for stat-intensive scenario
    
    * apitest: update duration
    
    * fs: remove "NodeSelector", add checks to memoryfs, passed simple test
    
    * test: update apitest
    
    * debug: remove a debug stmt

---
+++ src/backend/src/modules/puterfs/customfs/README.md
@@ -0,0 +1,15 @@
+# Custom FS Providers
+
+This directory contains custom FS providers that are not part of the core PuterFS.
+
+## MemoryFSProvider
+
+This is a demo FS provider that illustrates how to implement a custom FS provider.
+
+## NullFSProvider
+
+A FS provider that mimics `/dev/null`.
+
+## LinuxFSProvider
+
+Provide the ability to mount a Linux directory as a FS provider.
---
+++ src/backend/src/services/MemoryStorageService.js
@@ -0,0 +1,42 @@
+// METADATA // {"ai-commented":{"service":"mistral","model":"mistral-large-latest"}}
+/*
+ * Copyright (C) 2024-present Puter Technologies Inc.
+ *
+ * This file is part of Puter.
+ *
+ * Puter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+const BaseService = require("./BaseService");
+const { MemoryFSProvider } = require("../modules/puterfs/customfs/MemoryFSProvider");
+const { Readable } = require("stream");
+
+class MemoryStorageService extends BaseService {
+    async _init () {
+        console.log('MemoryStorageService._init');
+
+        const svc_mountpoint = this.services.get('mountpoint');
+        svc_mountpoint.set_storage(MemoryFSProvider, this);
+    }
+
+    async create_read_stream (uuid, options) {
+        const memory_file = options?.memory_file;
+        if ( ! memory_file ) {
+            throw new Error('MemoryStorageService.create_read_stream: memory_file is required');
+        }
+
+        return Readable.from(memory_file.content);
+    }
+}
+
+module.exports = MemoryStorageService;
