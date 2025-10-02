54b8990c706d87e3d10b1111650d3709ab173729
KernelDeimos <7225168+KernelDeimos@users.noreply.github.com>
2025-10-02 16:53:59 -0400

    devex(extensions): simplify kv get and set methods
    The basic `.get()` and `.set()` calls should be the same between
    persistent key-value store (puter-kvstore) and in-memory cache (kv.js).
    This commit wraps these methods to support the "shorthand" usage.

--- extensions/data.js
+++ extensions/data.js
@@ -4,8 +4,29 @@ const { DB_WRITE } = extension.import('core').database;
 const svc_database = extension.import('service:database');
 const svc_kvstore = extension.import('service:puter-kvstore');
 
+// Methods on the object from `.as()` come from TraitsFeature.js,
+// and they are already bound to their respective instance.
+const simplified_kv = { ...svc_kvstore.as('puter-kvstore') };
+
+const original_get = simplified_kv.get;
+const original_set = simplified_kv.set;
+
+simplified_kv.get = (...a) => {
+    if ( typeof a[0] === 'string' ) {
+        return original_get({ key: a[0] });
+    }
+    return original_get(...a);
+};
+
+simplified_kv.set = (...a) => {
+    if ( typeof a[0] === 'string' ) {
+        return original_set({ key: a[0], value: a[1] });
+    }
+    return original_set(...a);
+};
+
 extension.exports = {
     db: svc_database.get(DB_WRITE, 'extensions'),
-    kv: svc_kvstore.as('puter-kvstore'),
+    kv: simplified_kv,
     cache: kv,
 };
