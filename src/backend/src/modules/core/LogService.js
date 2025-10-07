70cde54f8060679f053db858d83865689ee3944f
Nariman Jelveh <nj@puter.com>
2025-10-06 22:16:53 -0700

    Update LogService.js

--- src/backend/src/modules/core/LogService.js
+++ src/backend/src/modules/core/LogService.js
@@ -449,7 +449,7 @@ class LogService extends BaseService {
 
         let logger;
 
-        if ( ! config.no_winston && false )
+        if ( ! config.no_winston )
         logger = new WinstonLogger(
             winston.createLogger({
                 levels: WINSTON_LEVELS,
