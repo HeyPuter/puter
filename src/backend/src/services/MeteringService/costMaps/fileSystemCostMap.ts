f2265cca594bdaad814742f7a0476052cfb0e65c
Daniel Salazar <daniel.salazar@puter.com>
2025-10-20 16:51:31 -0700

    Revert "Revert "Revert "feat: alert when going over usage limits significantl…" (#1787)
    This reverts commit 0d0cdce1cfb525adee8c8d90f7aaa717acf5aa1f.

--- src/backend/src/services/MeteringService/costMaps/fileSystemCostMap.ts
+++
@@ -1,8 +0,0 @@
-import { toMicroCents } from '../utils';
-
-export const FILE_SYSTEM_COST_MAP = {
-    'filesystem:ingress:bytes': 0,
-    'filesystem:delete:bytes': 0,
-    'filesystem:egress:bytes': toMicroCents(0.12 / 1024 / 1024 / 1024), // $0.11 per GB ~> 0.12 per GiB
-    'filesystem:cached-egress:bytes': toMicroCents(0.1 / 1024 / 1024 / 1024), // $0.09 per GB ~> 0.1 per GiB,
-};
--- src/backend/src/services/MeteringService/subPolicies/tempUserFreePolicy.ts
+++ src/backend/src/services/MeteringService/subPolicies/tempUserFreePolicy.ts
@@ -1,4 +1,4 @@
-import { toMicroCents } from '../utils';
+import { toMicroCents } from "../utils";
 
 export const TEMP_USER_FREE = {
     id: 'temp_free',
