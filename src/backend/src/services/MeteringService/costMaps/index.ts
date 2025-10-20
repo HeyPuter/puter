27f24118f34129e4083d4ba10b6d57fce4456593
Daniel Salazar <daniel.salazar@puter.com>
2025-10-20 13:54:21 -0700

    feat: alert when going over usage limits significantly + egress metering (#1782)
    * feat: alert when going over usage limits significantly + meter egress/ingress
    
    * fix: metering roundings

--- src/backend/src/services/MeteringService/costMaps/index.ts
+++ src/backend/src/services/MeteringService/costMaps/index.ts
@@ -1,16 +1,17 @@
-import { AWS_POLLY_COST_MAP } from "./awsPollyCostMap";
-import { AWS_TEXTRACT_COST_MAP } from "./awsTextractCostMap";
-import { CLAUDE_COST_MAP } from "./claudeCostMap";
-import { DEEPSEEK_COST_MAP } from "./deepSeekCostMap";
-import { GEMINI_COST_MAP } from "./geminiCostMap";
-import { GROQ_COST_MAP } from "./groqCostMap";
-import { KV_COST_MAP } from "./kvCostMap";
-import { MISTRAL_COST_MAP } from "./mistralCostMap";
-import { OPENAI_COST_MAP } from "./openAiCostMap";
-import { OPENAI_IMAGE_COST_MAP } from "./openaiImageCostMap";
-import { OPENROUTER_COST_MAP } from "./openrouterCostMap";
-import { TOGETHER_COST_MAP } from "./togetherCostMap";
-import { XAI_COST_MAP } from "./xaiCostMap";
+import { AWS_POLLY_COST_MAP } from './awsPollyCostMap';
+import { AWS_TEXTRACT_COST_MAP } from './awsTextractCostMap';
+import { CLAUDE_COST_MAP } from './claudeCostMap';
+import { DEEPSEEK_COST_MAP } from './deepSeekCostMap';
+import { FILE_SYSTEM_COST_MAP } from './fileSystemCostMap';
+import { GEMINI_COST_MAP } from './geminiCostMap';
+import { GROQ_COST_MAP } from './groqCostMap';
+import { KV_COST_MAP } from './kvCostMap';
+import { MISTRAL_COST_MAP } from './mistralCostMap';
+import { OPENAI_COST_MAP } from './openAiCostMap';
+import { OPENAI_IMAGE_COST_MAP } from './openaiImageCostMap';
+import { OPENROUTER_COST_MAP } from './openrouterCostMap';
+import { TOGETHER_COST_MAP } from './togetherCostMap';
+import { XAI_COST_MAP } from './xaiCostMap';
 
 export const COST_MAPS = {
     ...AWS_POLLY_COST_MAP,
@@ -26,4 +27,5 @@ export const COST_MAPS = {
     ...OPENROUTER_COST_MAP,
     ...TOGETHER_COST_MAP,
     ...XAI_COST_MAP,
+    ...FILE_SYSTEM_COST_MAP,
 };
