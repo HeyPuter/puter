eccc5e0c1f47f08fbe1bb843d2f7944d385b2c38
Daniel Salazar <daniel.salazar@puter.com>
2025-10-21 16:30:41 -0700

    chore: expose meteringService methods in wrapper while not in an extension

--- src/backend/src/services/MeteringService/MeteringServiceWrapper.mjs
+++ src/backend/src/services/MeteringService/MeteringServiceWrapper.mjs
@@ -12,5 +12,11 @@ export class MeteringServiceWrapper extends BaseService {
             alarmService: this.services.get('alarm'),
             eventService: this.services.get('event'),
         });
+        // TODO DS: if we can pull this to an extension I don't need this
+        // for now this is util so you don't have to extract this.meteringService
+        Object.getOwnPropertyNames(MeteringService.prototype).forEach(fn => {
+            if ( fn === 'constructor' ) return;
+            this[fn] = (...args) => this.meteringService[fn](args);
+        });
     }
 }
