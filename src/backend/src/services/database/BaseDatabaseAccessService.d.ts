045ef0a4e780d84a976317e8cb6d5fb075355ef5
Daniel Salazar <daniel.salazar@puter.com>
2025-11-05 16:32:36 -0800

    chore: type fixes (#1920)

--- src/backend/src/services/database/BaseDatabaseAccessService.d.ts
+++ src/backend/src/services/database/BaseDatabaseAccessService.d.ts
@@ -16,14 +16,14 @@ export interface IBaseDatabaseAccessService {
 export class BaseDatabaseAccessService extends BaseService implements IBaseDatabaseAccessService {
     static DB_WRITE: DBMode;
     static DB_READ: DBMode;
-    case<T>(choices: Record<string, T>): T;
-    get(): this;
-    read(query: string, params?: any[]): Promise<any>;
-    tryHardRead(query: string, params?: any[]): Promise<any>;
-    requireRead(query: string, params?: any[]): Promise<any>;
-    pread(query: string, params?: any[]): Promise<any>;
-    write(query: string, params?: any[]): Promise<any>;
-    insert(table_name: string, data: Record<string, any>): Promise<any>;
-    batch_write(statements: string[]): any;
-    _gen_insert_sql(table_name: string, data: Record<string, any>): string;
+    case<T>(choices: Record<'mysql' | 'sqlite' | (string & {}), T>): T;
+    get (): this;
+    read (query: string, params?: any[]): Promise<any>;
+    tryHardRead (query: string, params?: any[]): Promise<any>;
+    requireRead (query: string, params?: any[]): Promise<any>;
+    pread (query: string, params?: any[]): Promise<any>;
+    write (query: string, params?: any[]): Promise<any>;
+    insert (table_name: string, data: Record<string, any>): Promise<any>;
+    batch_write (statements: string[]): any;
+    _gen_insert_sql (table_name: string, data: Record<string, any>): string;
 }
