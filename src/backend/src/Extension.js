5f6bfce0bbe349c3833c46b379abf1191672ea59
KernelDeimos <7225168+KernelDeimos@users.noreply.github.com>
2025-11-06 23:16:59 -0500

    dev(extensions): expose `extension.errors` API

--- src/backend/src/Extension.js
+++ src/backend/src/Extension.js
@@ -23,6 +23,8 @@ const { Context } = require('./util/context');
 const { ExtensionServiceState } = require('./ExtensionService');
 const { display_time } = require('@heyputer/putility/src/libs/time');
 
+let memoized_errors = null;
+
 /**
  * This class creates the `extension` global that is seen by Puter backend
  * extensions.
@@ -139,6 +141,12 @@ class Extension extends AdvancedBase {
                 'initialized');
         }
         return log_context;
+    }
+    
+    get errors () {
+        return memoized_errors ?? (() => {
+            return this.services.get('error-service').create(this.log_context);
+        })();
     }
 
     /**
