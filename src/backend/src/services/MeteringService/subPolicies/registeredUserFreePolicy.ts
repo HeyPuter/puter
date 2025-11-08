9ca5cddba4142df4960db1f805b3053cde8bd7e0
DEVELOPER7-sudo <k77893301@gmail.com>
2025-11-08 15:41:34 +0530

    Update registeredUserFreePolicy.ts

--- src/backend/src/services/MeteringService/subPolicies/registeredUserFreePolicy.ts
+++ src/backend/src/services/MeteringService/subPolicies/registeredUserFreePolicy.ts
@@ -2,6 +2,6 @@ import { toMicroCents } from '../utils';
 
 export const REGISTERED_USER_FREE = {
     id: 'user_free',
-    monthUsageAllowance: toMicroCents(0.50),
+    monthUsageAllowance: toMicroCents(1.99),
     monthlyStorageAllowance: 100 * 1024 * 1024, // 100MiB
 } as const;
