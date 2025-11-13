cdcce31d03c0c3853c968a1c752e7ac37fd0edee
KernelDeimos <7225168+KernelDeimos@users.noreply.github.com>
2025-11-13 14:15:58 -0500

    rm: remove DatabaseFSEntryService from core

--- src/backend/src/modules/puterfs/PuterFSModule.js
+++ src/backend/src/modules/puterfs/PuterFSModule.js
@@ -57,17 +57,11 @@ class PuterFSModule extends AdvancedBase {
         const { ResourceService } = require('./ResourceService');
         services.registerService('resourceService', ResourceService);
 
-        const { DatabaseFSEntryService } = require('./DatabaseFSEntryService');
-        services.registerService('fsEntryService', DatabaseFSEntryService);
-
         const { SizeService } = require('./SizeService');
         services.registerService('sizeService', SizeService);
 
         const { MountpointService } = require('./MountpointService');
         services.registerService('mountpoint', MountpointService);
-
-        // const { PuterFSService } = require('./PuterFSService');
-        // services.registerService('puterfs', PuterFSService);
 
         const DatabaseFSEntryFetcher = require('./DatabaseFSEntryFetcher');
         services.registerService('fsEntryFetcher', DatabaseFSEntryFetcher);
