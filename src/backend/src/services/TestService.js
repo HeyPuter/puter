4da716d6319287566085b061b0a906d2b30b09b3
KernelDeimos <7225168+KernelDeimos@users.noreply.github.com>
2025-11-24 21:46:22 -0500

    dev(test): enable use of TestKernel in unit tests

---
+++ src/backend/src/services/TestService.js
@@ -0,0 +1,12 @@
+const BaseService = require('./BaseService');
+
+/**
+ * TestService is a service for testing in the sense that it is a service
+ * that exists for the purpose of being testing or to be used for testing
+ * purposes. However, TestService is not a service that's meant to hold
+ * utility functions for testing.
+ */
+class TestService extends BaseService {
+}
+
+module.exports = { TestService };
