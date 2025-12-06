ab0dd21f0c24908ac8533e79f12a21932e5d43d3
jelveh <nj@puter.com>
2025-12-06 12:02:14 -0800

    Update UIWindowLogin.js

--- src/gui/src/UI/UIWindowLogin.js
+++ src/gui/src/UI/UIWindowLogin.js
@@ -466,12 +466,6 @@ async function UIWindowLogin (options) {
         $(el_window).find('.signup-c2a-clickable').on('click', async function (e) {
             //destroy this window
             $(el_window).close();
-
-            if(!options.window_options){
-                options.window_options = {};
-            }
-            options.window_options.dominant = true;
-            options.window_options.center = true;
             // create Signup window
             const signup = await UIWindowSignup({
                 referrer: options.referrer,
