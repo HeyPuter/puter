c5dc68657bef306a9432e1774524a953f8411936
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-12-13 01:16:45 +0700

    Fix hosting types

--- src/puter-js/types/modules/hosting.d.ts
+++ src/puter-js/types/modules/hosting.d.ts
@@ -1,23 +1,20 @@
-import type { RequestCallbacks } from '../shared.d.ts';
 import type { FSItem } from './fs-item.d.ts';
 
-export interface Subdomain extends RequestCallbacks<Subdomain> {
+export interface Subdomain {
     uid: string;
     subdomain: string;
-    root_dir?: FSItem | string | null;
+    root_dir: FSItem;
 }
 
 export class Hosting {
-    constructor (context: { authToken?: string; APIOrigin: string; appID?: string });
-
-    setAuthToken (authToken: string): void;
-    setAPIOrigin (APIOrigin: string): void;
-
     list (): Promise<Subdomain[]>;
-    create (subdomain: string): Promise<Subdomain>;
-    create (subdomain: string, dirPath: string): Promise<Subdomain>;
-    create (options: { subdomain: string; root_dir?: string | FSItem }): Promise<Subdomain>;
+
+    create (subdomain: string, dirPath?: string): Promise<Subdomain>;
+    create (options: { subdomain: string; root_dir?: string }): Promise<Subdomain>;
+    
     update (subdomain: string, dirPath?: string | null): Promise<Subdomain>;
+
     get (subdomain: string): Promise<Subdomain>;
+
     delete (subdomain: string): Promise<boolean>;
 }
