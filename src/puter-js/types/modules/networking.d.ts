af309b0efa8d0e3862df0f042c1be8dacd4bf5ed
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-12-15 14:36:13 +0700

    Fix networking types

--- src/puter-js/types/modules/networking.d.ts
+++ src/puter-js/types/modules/networking.d.ts
@@ -8,26 +8,26 @@ export type SocketEvent =
     | 'tlsopen'
     | 'tlsclose';
 
-export class PSocket {
+export class Socket {
     constructor (host: string, port: number);
     write (data: ArrayBuffer | ArrayBufferView | string, callback?: () => void): void;
     close (): void;
     on (event: 'open', handler: () => void): void;
     on (event: 'data', handler: (buffer: Uint8Array) => void): void;
-    on (event: 'error', handler: (reason: unknown) => void): void;
+    on (event: 'error', handler: (reason: string) => void): void;
     on (event: 'close', handler: (hadError: boolean) => void): void;
     addListener (event: SocketEvent, handler: (...args: unknown[]) => void): void;
 }
 
-export class PTLSSocket extends PSocket {
+export class TLSSocket extends Socket {
     constructor (host: string, port: number);
 }
 
 export interface Networking {
     generateWispV1URL(): Promise<string>;
-    Socket: typeof PSocket;
+    Socket: typeof Socket;
     tls: {
-        TLSSocket: typeof PTLSSocket;
+        TLSSocket: typeof TLSSocket;
     };
     fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
 }
