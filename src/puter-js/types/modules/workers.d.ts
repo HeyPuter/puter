e0dbe0319502c9dfb9c33d2f0871014aad543397
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-12-15 18:36:32 +0700

    Fix worker types

--- src/puter-js/types/modules/workers.d.ts
+++ src/puter-js/types/modules/workers.d.ts
@@ -4,19 +4,15 @@ export interface WorkerInfo {
     file_path?: string;
     file_uid?: string;
     created_at?: string;
-    [key: string]: unknown;
 }
 
 export interface WorkerDeployment {
     success: boolean;
     url: string;
     errors?: unknown[];
-    [key: string]: unknown;
 }
 
 export class WorkersHandler {
-    constructor (authToken?: string);
-
     create (workerName: string, filePath: string, appName?: string): Promise<WorkerDeployment>;
     delete (workerName: string): Promise<boolean>;
     exec (request: RequestInfo | URL, init?: RequestInit): Promise<Response>;
