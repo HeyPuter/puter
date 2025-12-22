dc5f61b4384b0d2a3ba8b9eb82c9576463d4d03a
jelveh <nj@puter.com>
2025-12-22 08:32:51 -0800

    Add screen-wake-lock to iframe allow attribute

--- src/gui/src/UI/UIWindow.js
+++ src/gui/src/UI/UIWindow.js
@@ -404,7 +404,7 @@ async function UIWindow (options) {
                 style="${!options.has_head ? ' height: 100%;' : ''}">`;
     // iframe, for apps
     if ( options.iframe_url || options.iframe_srcdoc ) {
-        let allow_str = 'picture-in-picture; document-picture-in-picture; camera; encrypted-media; gamepad; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write; fullscreen; web-share; file-system-handle; local-storage; downloads;';
+        let allow_str = 'screen-wake-lock; picture-in-picture; document-picture-in-picture; camera; encrypted-media; gamepad; display-capture; geolocation; gyroscope; microphone; midi; clipboard-read; clipboard-write; fullscreen; web-share; file-system-handle; local-storage; downloads;';
         if ( window.co_isolation_enabled )
         {
             allow_str += ' cross-origin-isolated;';
