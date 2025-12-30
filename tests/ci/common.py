115b27a7f6ffc0ed1774655f9edda8b95e390e0f
Daniel Salazar <daniel.salazar@puter.com>
2025-12-29 20:06:21 -0500

    fix: don't cache nor fetch raw b64 url for app icons

--- tests/ci/common.py
+++ tests/ci/common.py
@@ -30,7 +30,7 @@ def get_admin_password() -> str:
     """
     Get the admin password from the backend server, throw an error if not found.
     """
-    for attempt in range(120):  # wait up to 60 seconds (1 minute)
+    for attempt in range(300):  # wait up to 60 seconds (1 minute)
         time.sleep(1)
         
         # read the log file
@@ -43,7 +43,7 @@ def get_admin_password() -> str:
                 print(f"Extracted admin password: {admin_password}")
                 return admin_password
     
-    raise RuntimeError(f"no admin password found after 120 seconds")
+    raise RuntimeError(f"no admin password found after 300 seconds")
 
 
 def get_token(admin_password: str) -> str:
