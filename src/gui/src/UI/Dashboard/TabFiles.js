aeb81c7eaca518011922da7e1eeced6a55146955
Miika Kuisma <miika.kuisma@icloud.com>
2026-01-22 12:12:56 +0200

    clearing out some linter errors

--- src/gui/src/UI/Dashboard/TabFiles.js
+++ src/gui/src/UI/Dashboard/TabFiles.js
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/* eslint-disable no-invalid-this */
+
 const folderIcon = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>';
 const documentIcon = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline></svg>';
 
@@ -110,7 +112,6 @@ const TabFiles = {
         });
 
         $('.files-tab .files .row').each(function () {
-            const _this = this;
             this.onclick = () => {
                 if ( this.parentElement.querySelector('.header') === this ) {
                     return; // Do not select header row
