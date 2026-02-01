22606f29b26f6c3c52e03ad39c143a8790db57ef
Neal Shah <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2026-02-01 15:03:30 -0500

    getAuthToken on node package (#2388)

--- src/puter-js/src/init.cjs
+++ src/puter-js/src/init.cjs
@@ -1,6 +1,8 @@
 const { readFileSync } = require('node:fs');
 const vm = require('node:vm');
 const { resolve } = require('node:path');
+const { IncomingMessage } = require('node:http');
+const open = require('open');
 /**
  * Method for loading puter.js in Node.js environment with auth token
  * @param {string} authToken - Optional auth token to initialize puter with
@@ -28,4 +30,22 @@ const init = (authToken) => {
     return goodContext.puter;
 };
 
-module.exports = { init };
+const getAuthToken = (guiOrigin = 'https://puter.com') => {
+    const http = require('http');
+
+    return new Promise((resolve) => {
+        const requestListener = function (/**@type {IncomingMessage} */ req, res) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Authentication Granted! You maty now close this window.');
+
+            resolve(new URL(req.url, 'http://localhost/').searchParams.get('token'));
+        };
+        const server = http.createServer(requestListener);
+        server.listen(0, function () {
+            const url = `${guiOrigin}/?action=authme&redirectURL=${encodeURIComponent('http://localhost:') + this.address().port}`;
+            open.default(url);
+        });
+    });
+};
+
+module.exports = { init, getAuthToken };
