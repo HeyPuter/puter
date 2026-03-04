80cabca7ad7240d1368f4443a23ca75bc44e5925
jelveh <nj@puter.com>
2026-03-03 19:31:39 -0800

    Add Google SVG icon to sign-in button

--- src/gui/src/UI/UIWindowLogin.js
+++ src/gui/src/UI/UIWindowLogin.js
@@ -85,7 +85,10 @@ async function UIWindowLogin (options) {
         h += '</form>';
         h += '<div class="oidc-providers-wrapper" style="display:none; padding: 0 0 10px 0;">';
         h += `<div style="text-align:center; margin: 10px 0; font-size:13px; color:var(--color-text-muted);">${ i18n('or') }</div>`;
-        h += `<button type="button" class="oidc-google-btn button button-block button-normal" style="display:flex; align-items:center; justify-content:center; gap:8px;">${i18n('sign_in_with_google')}</button>`;
+        h += `<button type="button" class="oidc-google-btn button button-block button-normal" style="display:flex; align-items:center; justify-content:center; gap:8px;">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+  <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
+</svg>${i18n('sign_in_with_google')}</button>`;
         h += '</div>';
         h += '</div>';
         // create account link
