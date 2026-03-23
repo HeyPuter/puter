d7e931af2d9918ee4bd0f321124caacf2aed75ec
Shubham Makkar <13jerry02@gmail.com>
2026-03-23 12:27:29 +0530

    feat: add System Information window

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SystemInformation from './SystemInformation';
+
+function App() {
+  return (
+    <div className="app">
+      <SystemInformation />
+    </div>
+  );
+}
+
+export default App;
