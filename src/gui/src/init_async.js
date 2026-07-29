92193ff11f54181e9df1feda2e3312bbae4345a2
Daniel Salazar <daniel.salazar@puter.com>
2026-07-28 19:02:49 -0700

    chore: kill collector (#3466)

--- src/gui/src/init_async.js
+++ src/gui/src/init_async.js
@@ -21,7 +21,6 @@ logger.info('start -> async initialization');
 
 import './util/TeePromise.js';
 import './util/Component.js';
-import './util/Collector.js';
 import './UI/UIElement.js';
 import './UI/UIWindowSaveAccount.js';
 import './UI/UIWindowEmailConfirmationRequired.js';
